9079bd87432cd354e4a67049d35aa03f0ab096b1
hetmodi03 <hetmodi03@gmail.com>
2025-11-20 21:53:13 +0530

    Revert "Add files via upload"

--- app.js
+++ app.js
@@ -1,89 +1,36 @@
 const path = require('path');
 
+const sequelize = require('./util/database');
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// new version of express.js
+// const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
 
 const app = express();
-const store = new SequelizeStore({
-  db: sequelize
-});
-
-store.sync();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'mysecret', resave: false, saveUninitialized: false , store: store}));
-
-// expose session auth flag to views
-app.use((req, res, next) => {
-  req.isLoggedIn = req.session ? !!req.session.isLoggedIn : false;
-  res.locals.isAuthenticated = req.isLoggedIn;
-  next();
-});
-  
-app.use((req, res, next) => {
-  req.isLoggedIn = req.session ? !!req.session.isLoggedIn : false;
-  res.locals.isAuthenticated = req.isLoggedIn; // available in all views
-  next();
-});
-
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force : true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then(user => {
-    return user;
-  })
- 
-  .then(result => {
-    app.listen(3001);
-  })
-  .catch(err => {
+sequelize.sync().then(result => {
+    console.log(result);
+    app.listen(3002);
+}).catch(err => {
     console.log(err);
-  });
+});
+
+
